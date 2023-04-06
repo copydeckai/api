a835ef4e152b481b9ae1d2c163de4f06e0ff1c72
Abednego Edet <ever.asap@gmail.com>
2023-04-06 21:51:50 +0100

    Update auth.js

--- dist/js/routes/auth.js
+++ dist/js/routes/auth.js
@@ -8,6 +8,7 @@ router.post("/register", auth_1.register);
 router.post("/login", auth_1.login);
 router.get("/fetch", auth_1.getUserDetails);
 router.post("/forgot-password", auth_1.forgotPassword);
+router.post("/resend-confirm", auth_1.resendConfirmEmail);
 router.get("/reset-password/:id/:token", auth_1.resetPassword);
 router.post("/reset-password/:id/:token", auth_1.resetPasswordPost);
 router.post("/change-password", verifyToken_1.verifyUser, auth_1.changePassword);
